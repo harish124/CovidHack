c2b79dfc39f1786877d1fe1754ed6b026a93699b
Clement Smith <rclemsmith@gmail.com>
2020-04-27 22:08:05 +0530

    "First Commit by Clement"

---
+++ app/src/main/java/com/example/CovidHack/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.example.CovidHack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Main2Activity extends AppCompatActivity {
+
+    FirebaseAuth mauth ;
+    EditText name;
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mauth = FirebaseAuth.getInstance();
+        FirebaseUser user = mauth.getCurrentUser();
+        name = findViewById(R.id.name);
+
+        TextView tv = findViewById(R.id.set);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        user newuser = new user();
+        newuser.setName("Clement");
+        newuser.setAddress("Thuraipakkam");
+
+//        mDatabase.child("users").child(user.getUid()).setValue(newuser);
+
+        Intent i = new Intent(Main2Activity.this,YoutubeList.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/CovidHack/videodetails.java
@@ -0,0 +1,48 @@
+package com.example.CovidHack;
+
+public class videodetails {
+
+    public  String videoId,title,description,url;
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public videodetails() {
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public videodetails(String videoId, String title, String description, String url) {
+        this.videoId = videoId;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+    }
+}
