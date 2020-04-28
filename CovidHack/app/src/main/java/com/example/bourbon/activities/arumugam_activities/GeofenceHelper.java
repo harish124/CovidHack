ca2fce731b60d1dfc813631b338422c5535dbbc3
Aaru-06 <arumugampillai0610@gmail.com>
2020-04-28 13:27:11 +0530

    Arumugam's files

---
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/arumugam_activities/GeofenceHelper.java
@@ -0,0 +1,73 @@
+package com.arumugam.geofencing;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.location.Location;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingRequest;
+
+public class GeofenceHelper extends ContextWrapper {
+
+    private static final String TAG = "Geofencing";
+    PendingIntent pendingIntent;
+
+    public GeofenceHelper(Context base) {
+        super(base);
+    }
+
+    public GeofencingRequest getGeofencingRequest(Geofence geofence) {
+        return new GeofencingRequest.Builder()
+                .addGeofence(geofence)
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                .build();
+    }
+
+    public Geofence getGeofence(String ID, Location latLng, float radius, int transitionTypes) {
+        if(latLng==null)
+        {
+            Log.d("Error","location null");
+        }
+        return new Geofence.Builder()
+                .setCircularRegion(latLng.getLatitude(), latLng.getLongitude(), radius)
+                .setRequestId(ID)
+                .setTransitionTypes(transitionTypes)
+                .setLoiteringDelay(5000)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build();
+    }
+
+    public PendingIntent getPendingIntent() {
+        if (pendingIntent != null) {
+            return pendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 2607, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return pendingIntent;
+    }
+
+    public String getErrorString(Exception e) {
+        if (e instanceof ApiException) {
+            ApiException apiException = (ApiException) e;
+            switch (apiException.getStatusCode()) {
+                case GeofenceStatusCodes
+                        .GEOFENCE_NOT_AVAILABLE:
+                    return "GEOFENCE_NOT_AVAILABLE";
+                case GeofenceStatusCodes
+                        .GEOFENCE_TOO_MANY_GEOFENCES:
+                    return "GEOFENCE_TOO_MANY_GEOFENCES";
+                case GeofenceStatusCodes
+                        .GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                    return "GEOFENCE_TOO_MANY_PENDING_INTENTS";
+            }
+        }
+        return e.getLocalizedMessage();
+    }
+}
