f9b850f788f0a806672619edc0bb911d3fbeedda
Clement Smith <rclemsmith@gmail.com>
2020-04-29 21:08:43 +0530

    User Detail UI

---
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/clement_activities/User_Registration.java
@@ -0,0 +1,16 @@
+package com.example.bourbon.activities.clement_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bourbon.R;
+
+public class User_Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data);
+    }
+}
