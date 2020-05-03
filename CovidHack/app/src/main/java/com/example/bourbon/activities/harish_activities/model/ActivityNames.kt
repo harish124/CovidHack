9f4c9d4128902da54090aab8de2ea56a9a11d0a9
harish124 <balajiharish4@gmail.com>
2020-05-03 21:14:11 +0530

    Added kotlin dependencies and added ShopInfo RV etc

---
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/harish_activities/model/ActivityNames.kt
@@ -0,0 +1,14 @@
+package com.example.bourbon.activities.harish_activities.model
+
+import frame_transition.Transition
+
+class ActivityNames {
+    var classNames=ArrayList<String>()
+
+    init {
+
+        classNames.add("Main_menu.class")
+
+    }
+
+}
