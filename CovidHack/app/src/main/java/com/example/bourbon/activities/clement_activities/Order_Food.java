662fa3aeae28e86b793b0bed187f9f22c8694a21
Clement Smith <rclemsmith@gmail.com>
2020-05-06 10:39:27 +0530

    Add to Card

--- CovidHack/app/src/main/java/com/example/bourbon/activities/clement_activities/Order_Food.java
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/clement_activities/Order_Food.java
@@ -1,5 +1,8 @@
 package com.example.bourbon.activities.clement_activities;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,18 +10,17 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bourbon.R;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import print.Print;
 
 public class Order_Food extends AppCompatActivity {
 
@@ -31,7 +33,10 @@ public class Order_Food extends AppCompatActivity {
 
     ListView groceryList;
     private ArrayAdapter<String> adapter;
-    private ArrayList<String> groceries  = new ArrayList<String>();
+    private ArrayList<String> groceries = new ArrayList<String>();
+    private String[] groceries1 ;
+    AlertDialog.Builder builder ;
+    Print p = new Print(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +45,42 @@ public class Order_Food extends AppCompatActivity {
         setContentView(R.layout.activity_grocery_purchase);
         ButterKnife.bind(this);
         View view = LayoutInflater.from(getApplication()).inflate(R.layout.listing_view, null);
-        groceryList = findViewById(R.id.grocery_list);
+        groceryList = view.findViewById(R.id.grocery_list);
+        builder = new AlertDialog.Builder(Order_Food.this);
+        builder.setTitle("Cart");
         Bundle b = getIntent().getExtras();
         String name = b.getString("Name");
         storeName.setText(name);
-        adapter = new ArrayAdapter<String>(this,R.layout.gro_list,R.id.textViewgroc,groceries);
+        adapter = new ArrayAdapter<String>(this, R.layout.gro_list, R.id.textViewgroc, groceries);
         groceryList.setAdapter(adapter);
     }
 
-    @OnClick(R.id.add_item)
-    public void onViewClicked() {
-        groceries.add(itemName.getText().toString() + itemQuantity.getText().toString());
-        adapter.notifyDataSetChanged();
+
+    @OnClick({R.id.add_item, R.id.submit_list, R.id.cart})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.add_item:
+                groceries.add(itemName.getText().toString() + ":  " +  itemQuantity.getText().toString());
+                adapter.notifyDataSetChanged();
+                groceries1 = groceries.toArray(new String[groceries.size()]) ;
+                p.sprintf("Item Added To Cart");
+
+                break;
+            case R.id.submit_list:
+                break;
+            case R.id.cart:
+                builder.setItems(groceries1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        groceries.remove(which);
+                        groceries1 = groceries.toArray(new String[groceries.size()]) ;
+                        p.sprintf("Item Deleted From Cart");
+                    }
+
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
     }
 }
