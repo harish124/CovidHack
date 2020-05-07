8f855d941f2d39c061179d047cbe1c0023543a04
Clement Smith <rclemsmith@gmail.com>
2020-05-07 19:11:05 +0530

    Linking

--- CovidHack/app/src/main/java/com/example/bourbon/activities/clement_activities/Verifi.java
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/clement_activities/Verifi.java
@@ -139,9 +139,9 @@ public class Verifi extends AppCompatActivity {
                                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
                                     } else {
-//                                        Intent intent = new Intent(Verifi.this, User_Registration.class);
-//                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                                        startActivity(intent);
+                                        Intent intent = new Intent(Verifi.this, User_Registration.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
                                     }
                                 }
 
