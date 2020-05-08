68ea0e59fb2fcb74a46828adad400ca164e6f04d
harish124 <balajiharish4@gmail.com>
2020-05-08 19:15:44 +0530

    Added CustomerOrderInfo.kt

--- CovidHack/app/src/main/java/com/example/bourbon/activities/harish_activities/adapters/GenericRVAdapter.kt
+++ CovidHack/app/src/main/java/com/example/bourbon/activities/harish_activities/adapters/GenericRVAdapter.kt
@@ -1,20 +1,15 @@
 package com.example.bourbon.activities.harish_activities.adapters
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bourbon.R
-import com.example.bourbon.activities.clement_activities.Order_Details
 import com.example.bourbon.activities.harish_activities.model.CustomerOrder
-import com.example.bourbon.activities.harish_activities.model.Order
 import com.example.bourbon.databinding.CardCustomerOrderBinding
-import com.example.bourbon.databinding.CardOrderBinding
-import print.Print
 
-class GenericRVAdapter(var products:ArrayList<CustomerOrder>, binding:CardCustomerOrderBinding): RecyclerView.Adapter<GenericRVAdapter.MyViewHolder>() {
+class GenericRVAdapter(var products:ArrayList<CustomerOrder>): RecyclerView.Adapter<GenericRVAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView:CardCustomerOrderBinding,context:Context):RecyclerView.ViewHolder(itemView.root) {
         var binding:CardCustomerOrderBinding?=null
@@ -39,11 +34,11 @@ class GenericRVAdapter(var products:ArrayList<CustomerOrder>, binding:CardCustom
     }
 
     override fun getItemCount(): Int {
-        return products!!.size
+        return products.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder?.binding?.pd= products!!.get(position)
+        holder.binding?.pd= products.get(position)
 
     }
 }
